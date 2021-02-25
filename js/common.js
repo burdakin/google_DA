41594b7f65f5b0cd9983bb31da0dda7a48c0f934
burdakin <serjb94@gmail.com>
2021-02-25 11:35:32 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(function () {
+    $("#hamburger").click(function () {
+        $(this).toggleClass("is-active");
+
+        if ($(this).hasClass("is-active")) {
+            $("#services_window").slideDown(300);
+        } else {
+            $("#services_window").slideUp(300);
+        }
+    });
+
+    $(document).on("click", function (e) {
+        var container = $("#hamburger, #services_window");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $("#hamburger").toggleClass("is-active");
+            $("#services_window").slideUp(300);
+        }
+    });
+});
+
+function err() {
+alert('Хватит тебе, это же макет – иди погугли на настоящем Яндексе!')
+}
+
+var click = document.getElementById('search_input');
+click.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        err();
+    }
+})
+
+// Говорят, что keyCode ныне не используется, но я ничего другого не нашел(
