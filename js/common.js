b7424b86024119e700cd49c381225456d187ae5c
burdakin <serjb94@gmail.com>
2021-02-25 14:47:58 +0300

    Dropout updated

--- js/common.js
+++ js/common.js
@@ -30,3 +30,8 @@ click.addEventListener('keydown', function (e) {
 })
 
 // Говорят, что keyCode ныне не используется, но я ничего другого не нашел(
+
+var menu_disp = document.getElementById('services_window');
+window.addEventListener('load', function () {
+    menu_disp.style.display = 'none';
+})
